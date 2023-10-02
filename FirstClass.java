8bebbeff31c91adb0585ad25bbf8617052565120
Jerry <111554668+NotTheRussian@users.noreply.github.com>
2023-10-01 22:08:23 -0600

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,13 @@
+public class FirstClass {
+    public static void main(String[] args) {
+
+        boolean isCat = true;
+        if (isCat == true) {
+            System.out.println("It's a cat!");
+        }
+        else {
+            System.out.println("Is it a dog?");
+        }
+
+    }
+}
